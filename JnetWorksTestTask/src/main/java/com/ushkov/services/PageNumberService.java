43f7aa7c899b2fc2a21e8ff3eda931dc93324d34
Yegor Ushkov <exaltrse@gmail.com>
2021-10-05 08:23:49 +0300

    Initial commit. Design function name and names for unit tests. Add dependencies to pom.xml

---
+++ JnetWorksTestTask/src/main/java/com/ushkov/services/PageNumberService.java
@@ -0,0 +1,7 @@
+package com.ushkov.services;
+
+public class PageNumberService {
+    public static String reducePageNumbers(String pageNumbers){
+        return "";
+    }
+}
---
+++ JnetWorksTestTask/src/test/java/com/ushkov/PageNumberServiceJUnitTest.java
@@ -0,0 +1,29 @@
+package com.ushkov;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PageNumberServiceJUnitTest extends Assert {
+
+    public void reducePageNumbers_OneNumber_PositiveTest(){    }
+
+    public void reducePageNumbers_OneRange_PositiveTest(){    }
+
+    public void reducePageNumbers_WithoutRange_PositiveTest(){    }
+
+    public void reducePageNumbers_SortedList_PositiveTest(){    }
+
+    public void reducePageNumbers_UnsortedList_PositiveTest(){    }
+
+    public void reducePageNumbers_Duplicates_PositiveTest(){    }
+
+    public void reducePageNumbers_NullRequest_NegativeTest(){    }
+
+    public void reducePageNumbers_EmptyRequest_NegativeTest(){    }
+
+    public void reducePageNumbers_ZeroPageNumber_NegativeTest(){    }
+
+    public void reducePageNumbers_NegativePageNumber_NegativeTest(){    }
+
+    public void reducePageNumbers_NondigitPageNumber_NegativeTest(){    }
+}
